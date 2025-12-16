532b4dae24857ae32ccdb8bbc5b6f78f8d9cba95
Rohaan02 <rohaannadeem2@gmail.com>
2025-12-16 13:03:33 +0500

    added ngrok url at axiosConfig.

--- src/lib/axiosConfig.js
+++ src/lib/axiosConfig.js
@@ -1,8 +1,11 @@
 // src/lib/axiosConfig.js
 import axios from "axios";
 
+// ngrok url
 const API_BASE_URL = "https://f194cba6ecec.ngrok-free.app";
+// localhost url
 // const API_BASE_URL = "http://127.0.0.1:8000/api";
+// online url
 //const API_BASE_URL = "https://bemsolopex.devifyio.com/backend/public/api";
 
 const api = axios.create({

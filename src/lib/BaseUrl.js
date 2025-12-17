f40ad1be100d3d80a0980f7557aeed2895dd925f
Rohaan02 <rohaannadeem2@gmail.com>
2025-12-17 21:20:39 +0500

    changed ngrok url to check it on vercel.

--- src/lib/BaseUrl.js
+++ src/lib/BaseUrl.js
@@ -1,9 +1,9 @@
 // - - - - backend urls - - - -
 //online url
-const baseUrl = "https://1ef2ebdb6e88.ngrok-free.app/api";
+//const baseUrl = "https://bemsolopex.devifyio.com/backend/public/api";
 //localhost url
 // const baseUrl = "http://127.0.0.1:8000/api";
 // ngrok url:
-// const baseUrl = "https://f194cba6ecec.ngrok-free.app/api";
+const baseUrl = "https://1ef2ebdb6e88.ngrok-free.app/api";
 
 export default baseUrl;

8c5878225d3830fb0715142106972574822a369f
Rohaan02 <rohaannadeem2@gmail.com>
2025-12-17 21:14:04 +0500

    changed the baseUrl to ngrok url for testing online.

--- src/lib/BaseUrl.js
+++ src/lib/BaseUrl.js
@@ -1,8 +1,8 @@
 // - - - - backend urls - - - -
 //online url
-//const baseUrl = "https://bemsolopex.devifyio.com/backend/public/api";
+const baseUrl = "https://1ef2ebdb6e88.ngrok-free.app/api";
 //localhost url
-const baseUrl = "http://127.0.0.1:8000/api";
+// const baseUrl = "http://127.0.0.1:8000/api";
 // ngrok url:
 // const baseUrl = "https://f194cba6ecec.ngrok-free.app/api";
 
